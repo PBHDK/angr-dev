a39f349c7807b3d846e8ddc5d9f1a822c505dadb
Paul Heidekrüger <paul.heidekrueger@tum.de>
2023-06-29 22:23:59 +0000

    nix: use pypy3

--- shell.nix
+++ shell.nix
@@ -1,4 +1,4 @@
-with import <nixpkgs> { };
+with import (builtins.fetchTarball { url = "https://github.com/NixOS/nixpkgs/archive/60a783e00517fce85c42c8c53fe0ed05ded5b2a4.tar.gz"; }) { };
 stdenv.mkDerivation rec {
   name = "angr-env";
 
@@ -10,9 +10,7 @@ stdenv.mkDerivation rec {
   ];
 
   buildInputs = [
-    python3Packages.virtualenvwrapper
-    python2 # To build unicorn
-    python3 # For CPython install
+    bashInteractive
     pypy3 # for PyPy install
     nasm
     libxml2
@@ -25,17 +23,28 @@ stdenv.mkDerivation rec {
     pixman
     qt5.qtdeclarative
     openssl
-    jdk8
-    pkgs.z3
-    python3Packages.setuptools
-    pkgs.gdb
+    # jdk8
+    ((z3.override { python = pypy3; }).overrideAttrs
+      (finalAttrs: previousAttrs: {
+        postInstall = " 
+                              mkdir -p $dev $lib
+                              mv $out/lib $lib/lib
+                              mv $out/include $dev/include
+                              mkdir -p $python/lib
+                              mv $lib/lib/pypy* $python/lib/
+                              ln -sf $lib/lib/libz3${stdenv.hostPlatform.extensions.sharedLibrary} $python/${pypy3.sitePackages}/z3/lib/libz3${stdenv.hostPlatform.extensions.sharedLibrary}
+          		    ";
+      }))
+    # (z3.override { python = pypy3; })
+    pypy3Packages.setuptools
+    gdb
 
     # needed for pure environments
     which
   ];
 
   shellHook = ''
-    source $(command -v virtualenvwrapper.sh)
-    export LD_LIBRARY_PATH="${stdenv.cc.cc.lib}/lib:${pkgs.z3.lib}/lib:$LD_LIBRARY_PATH"
+    alias gdb-multiarch="aarch64-linux-unknown-gnu-gdb"
+    export LD_LIBRARY_PATH="${gcc-unwrapped.lib}/lib:${pkgs.z3.lib}/lib:$LD_LIBRARY_PATH"
   '';
 }
